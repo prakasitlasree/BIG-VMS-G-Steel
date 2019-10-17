7f2114407d42735caa3661e13b8e54bb3999a12b
prakasit <prakasitlasree@gmail.com>
2019-10-17 23:04:17 +0700

    update idcard

--- BIG.VMS.GS/BIG.VMS.PRESENT/Forms/FormVisitorNew/frmVisitorNew.cs
+++ BIG.VMS.GS/BIG.VMS.PRESENT/Forms/FormVisitorNew/frmVisitorNew.cs
@@ -460,7 +460,7 @@ namespace BIG.VMS.PRESENT.Forms.FormVisitor
                                     : "ไม่ระบุ";
                                 txtFirstName.Text = $@"กลุ่มพนักงานผู้รับเหมา {companyName}";
 
-                                txtIDCard.Text = $@"กลุ่มพนักงานผู้รับเหมา {companyName}";
+                                //txtIDCard.Text = $@"กลุ่มพนักงานผู้รับเหมา {companyName}"; comment ไว้ให้เค้า กรอกเองหรืออ่านบัตร
                                 txtEmployee.Text = $@"{TrnProjectMaster.RESPONSIBLE_MANAGER}";
                                 txtReason.Text = $@"{TrnProjectMaster.PROJECT_NAME}";
                             }
@@ -468,7 +468,7 @@ namespace BIG.VMS.PRESENT.Forms.FormVisitor
                             {
                                 txtFirstName.Text = $@"กลุ่มลูกค้า {TrnCustomerVisit.CUSTOMER_NAME}";
 
-                                txtIDCard.Text = $@"กลุ่มลูกค้า {TrnCustomerVisit.CUSTOMER_NAME}";
+                                //txtIDCard.Text = $@"กลุ่มลูกค้า {TrnCustomerVisit.CUSTOMER_NAME}"; comment ไว้ให้เค้า กรอกเองหรืออ่านบัตร
                                 txtEmployee.Text = $@"{TrnCustomerVisit.CONTACT_PERSON}";
                                 txtReason.Text = $@"{TrnCustomerVisit.OBJECTIVE_OF_VISIT}";
                             }
