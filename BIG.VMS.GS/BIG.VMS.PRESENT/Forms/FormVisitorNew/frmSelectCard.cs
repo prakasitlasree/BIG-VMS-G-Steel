11c544e9ca25ee7c13bf065d2571d0b24d3db835
prakasit <prakasitlasree@gmail.com>
2019-10-26 23:34:05 +0700

    1

--- BIG.VMS.GS/BIG.VMS.PRESENT/Forms/FormVisitorNew/frmSelectCard.cs
+++ BIG.VMS.GS/BIG.VMS.PRESENT/Forms/FormVisitorNew/frmSelectCard.cs
@@ -28,6 +28,27 @@ namespace BIG.VMS.PRESENT.Forms.FormVisitorNew
         public frmSelectCard()
         {
             InitializeComponent();
+
+            ListCardReader();
+            string fileName = CardHelper.StartupPath + "\\RDNIDLib.DLD";
+            if (System.IO.File.Exists(fileName) == false)
+            {
+                MessageBox.Show("RDNIDLib.DLD not found");
+            }
+
+            System.Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            this.Text = String.Format("R&D NID Card Plus C# {0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
+
+            byte[] _lic = CardHelper.String2Byte(fileName);
+
+            int nres = 0;
+            nres = RDNID.openNIDLibRD(_lic);
+            if (nres != 0)
+            {
+                String m;
+                m = String.Format(" error no {0} ", nres);
+                MessageBox.Show(m);
+            }
         }
 
         private void FrmSelectCard_Load(object sender, EventArgs e)
